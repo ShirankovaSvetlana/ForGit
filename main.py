c573a54e900c5873a2879f95871b87300194b6f0
ShirankovaSvetlana <sv_se@mail.ru>
2023-11-22 17:04:22 +0300

    Звуковой файл воспроизводится 1 раз

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('New one')
-    print('One more print')
+from pygame import mixer
+import time
+
+def play_sound(sound_file):
+    try:
+        mixer.init()
+        mixer.music.load("eng9_17112023_Final.mp3")
+        mixer.music.play()
+        while mixer.music.get_busy():  # wait for music to finish playing
+            time.sleep(1)
+    except Exception:
+        pass
 
 
-if __name__ == '__main__':
-    main()
+def start_playing():
+    with open("tries.txt") as f:
+        data = f.read()
+        is_playing = bool(data)
+
+    with open("tries.txt", mode='w') as f:
+        f.write('True')
+
+    if not is_playing:
+        sound_file = "eng9_17112023_Final.mp3"
+        play_sound(sound_file)
+
+
+if __name__ == "__main__":
+    start_playing()
